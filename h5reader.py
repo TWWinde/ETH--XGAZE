fd91bdd1758d1d1c776869b220146028cdee48bc
TWWinde <twwinde@gmail.com>
2024-02-27 14:00:41 +0100

    Update h5reader.py

--- h5reader.py
+++ h5reader.py
@@ -6,12 +6,13 @@ hdf5_file_path = '/datasets/public/eth-xgaze/preprocessed_224/xgaze_224/train/su
 with h5py.File(hdf5_file_path, 'r') as file:
 
     print("Keys: %s" % list(file.keys()))
-    cam_index = file['cam_index'][:]
-    face_gaze = file['face_gaze'][:]
-    face_head_pose = file['face_head_pose'][:]
-    face_mat_norm = file['face_mat_norm'][:]
-    face_patch = file['face_patch'][:]
-    frame_index = file['frame_index'][:]
+    #  Keys: ['cam_index', 'face_gaze', 'face_head_pose', 'face_mat_norm', 'face_patch', 'frame_index']
+    cam_index = file['cam_index'][1]
+    face_gaze = file['face_gaze'][1]
+    face_head_pose = file['face_head_pose'][1]
+    face_mat_norm = file['face_mat_norm'][1]
+    face_patch = file['face_patch'][1]
+    frame_index = file['frame_index'][1]
 
     # 打印数据以检查
     print("Camera Index:", cam_index)
