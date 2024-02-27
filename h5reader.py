8145f72c59ad9c64074a5ffabcf511b7e90f55fe
TWWinde <twwinde@gmail.com>
2024-02-27 13:23:29 +0100

    Update h5reader.py

--- h5reader.py
+++ h5reader.py
@@ -1,8 +1,8 @@
 import h5py
 
 
-hdf5_file_path = '/datasets/public/eth-xgaze/preprocessed_224/xgaze_224/train/subject0000.h5'
+hdf5_file_path = '/datasets/public/eth-xgaze/preprocessed_224/xgaze_224/train/subject0010.h5'
 
 with h5py.File(hdf5_file_path, 'r') as file:
-    # 列出文件中的顶级组/数据集
+
     print("Keys: %s" % list(file.keys()))
