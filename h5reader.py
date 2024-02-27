c0a8e309466091d20b00b143a0f968147b1055db
TWWinde <twwinde@gmail.com>
2024-02-27 14:59:40 +0100

    Update h5reader.py

--- h5reader.py
+++ h5reader.py
@@ -4,11 +4,11 @@ import h5py
 
 hdf5_file_path = '/datasets/public/eth-xgaze/preprocessed_224/xgaze_224/train/subject0010.h5'
 
-with open('/datasets/public/eth-xgaze/preprocessed_224/xgaze_224/readme.txt', 'r', encoding='utf-8') as file:
-    content = file.read()
+#with open('/datasets/public/eth-xgaze/preprocessed_224/xgaze_224/readme.txt', 'r', encoding='utf-8') as file:
+    #content = file.read()
 
 # 打印文件内容
-print(content)
+#print(content)
 
 with h5py.File(hdf5_file_path, 'r') as file:
 
